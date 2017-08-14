1d6913392e226bf5b660752561a16cbc0375bffb
xiaominglei001 <xiaominglei001@qq.com>
2017-08-14 13:08:21 +0800

    Test

---
+++ app/src/main/java/com/indicatordemo/Test1.java
@@ -0,0 +1,16 @@
+package com.indicatordemo;
+
+import android.app.Activity;
+import android.util.Log;
+
+/**
+ * Created by xiao on 2017/8/14.
+ */
+
+public class Test1 extends Activity {
+
+    public void Test(){
+        Log.v("Heoo","ddddeddddddeddddddde");
+    }
+
+}
