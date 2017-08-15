d3391f10c9e95c63aeec022c1bf176786d83efd4
xiaominglei001 <xiaominglei001@qq.com>
2017-08-15 09:52:15 +0800

    修改main

--- app/src/main/java/com/indicatordemo/MainActivity.java
+++ app/src/main/java/com/indicatordemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.indicatordemo;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         vp = (ViewPager) findViewById(R.id.vp);
         vpAdapter = new VPAdapter(this);
         vp.setAdapter(vpAdapter);
+
+        Log.e("111", "测试---------------");
 
         ll = (LinearLayout) findViewById(R.id.ll);
         vp.setOnPageChangeListener(new ViewPagerIndicator(this, vp, ll, 3));
